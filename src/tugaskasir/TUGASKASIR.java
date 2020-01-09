7f5e6fac4774efe506f6159b6ebc5f0776cbcd7e
nurulhikmahida24 <nurulhikmahidayat@gmail.com>
2020-01-09 23:35:12 +0700

    Perubahan

--- src/tugaskasir/TUGASKASIR.java
+++ src/tugaskasir/TUGASKASIR.java
@@ -1,22 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tugaskasir;
+
 import java.util.Scanner;
 
-/**
- *
- * @author Acer-PC
- */
+
 public class TUGASKASIR {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
         
         int a;
         Scanner sc = new Scanner(System.in);
